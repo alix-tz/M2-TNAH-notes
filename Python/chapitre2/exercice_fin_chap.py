a17ddcb5a4caaa378aac13be12ba5f81a6c42827
Alix Chagué <alix.chague@enc-sorbonne.fr>
2017-12-18 18:49:43 +0100

    ajout solution exercice fin du chapitre 2

---
+++ Python/chapitre2/exercice_fin_chap.py
@@ -0,0 +1,36 @@
+def ecrire_colonne(distribution, fichier):
+    """ Ecrit dans un fichier chaque mot en clef de distribution avec la fréquence associée
+    dans un fichier donné.
+    
+    :param distibution: Dictionnaire où la clef est un mot et la valeur le nombre d'occurrence
+    :type distribution: dict
+    :param fichier: Fichier ouvert pour l'écriture
+    :type fichier: TextIOWrapper
+    """
+    fichier.write("Mot;Distribution\n")
+    for mot, frequence in distribution.items():
+        fichier.write(mot + ";" + str(frequence) + '\n')
+
+# Ouvrir le fichier et stocker son contenu
+with open("data/horace.txt") as fichier_ecrit:
+    texte = fichier_ecrit.read()
+
+# Nettoyer le texte
+ponctuation = '!@#$%^&*()_+={}[]:;"\|<>,.?/~`\t'
+texte = texte.lower()
+for marqueur in ponctuation:
+    texte = texte.replace(marqueur, '')
+texte = texte.replace("'", " ").replace("-", " ").replace("\n"," ")
+
+# Calculer la distribution
+liste_mots = texte.split()
+
+dict_frequence = {}
+for mot in liste_mots:
+    if mot not in dict_frequence:
+        dict_frequence[mot]= liste_mots.count(mot)
+
+# Ouvrir le fichier `frequence_horace.txt` pour écrire
+with open("resultats/frequence_horace.txt", mode="w") as fichier_texte:
+# Utiliser la fonction ecrire_colonne pour écrire dans ce fichier
+    ecrire_colonne(dict_frequence, fichier_texte)
