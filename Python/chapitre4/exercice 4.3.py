923879f7c34889149f937c08f9061727993688f3
alix-tz <33317799+alix-tz@users.noreply.github.com>
2018-01-14 23:01:56 +0100

    màj après documentation de la fonction de l'ex 4.3

--- Python/chapitre4/exercice 4.3.py
+++ Python/chapitre4/exercice 4.3.py
@@ -1,9 +1,16 @@
 def iiif_csv(ark, nom_csv):
     colonnes = ["Numéro", "Nom de Page", "Lien image", "Largeur", "Longueur"]
     # Complétez avec la documentation
-    
-    
-    
+    """ Affiche les métadonnées d'un objet JSON à partir d'un identifiant ark, 
+    et stocke dans un fichier CSV des métadonnées précises.
+
+    :param ark: identifiant ark BNF
+    :type ark: string
+    :param nom_csv: chemin vers un fichier csv
+    :type nom_csv: string
+    :returns: None
+    """
+       
     import requests
     import csv
     # 1. afficher l'ensemble des métadonnées (?)
@@ -16,11 +23,12 @@ def iiif_csv(ark, nom_csv):
     # 2. générer un fichier CSV
     # préparer une liste des métadonnées sélectionnées
     pour_csv = []
-    pour_csv.append("1")
-    pour_csv.append(meta[6]["value"])
-    pour_csv.append(meta[3]["value"])
-    pour_csv.append(str(data["sequences"][0]["canvases"][0]["width"]))
-    pour_csv.append(str(data["sequences"][0]["canvases"][0]["height"]))
+    pour_csv.append("1") # numéro
+    # remarque : on pourrait améliorer le calcul de "numéro"
+    pour_csv.append(meta[6]["value"]) # nom du fichier
+    pour_csv.append(meta[3]["value"]) # image source
+    pour_csv.append(str(data["sequences"][0]["canvases"][0]["width"])) # largeur
+    pour_csv.append(str(data["sequences"][0]["canvases"][0]["height"])) # longeur
     
     # écrire dans le fichier CSV
     with open(nom_csv, mode="w") as file:
